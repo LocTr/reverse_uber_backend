528e34fcda9858d99af9da113db63aaad7aa791c
Loc <ttloc1999@gmail.com>
2024-08-14 23:55:39 +0700

    setting up

--- models/config.go
+++ models/config.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/go-ozzo/ozzo-validation/is"
+)
+
 type EnvConfig struct {
 	ServerPort      string `mapstructure:"SERVER_PORT"`
 	ServerAddress   string `mapstructure:"SERVER_ADDRESS"`
@@ -12,4 +17,19 @@ type EnvConfig struct {
 	JWTAccessExpirationMinutes int    `mapstructure:"JWT_ACCESS_EXPIRATION_MINUTES"`
 	JWTRefreshExpirationDays   int    `mapstructure:"JWT_REFRESH_EXPIRATION_DAYS"`
 	Mode                       string `mapstructure:"MODE"`
+}
+
+func (config *EnvConfig) Validate() error {
+	return validation.ValidateStruct(config,
+		validation.Field(&config.ServerPort, is.Port),
+		validation.Field(&config.ServerAddress, validation.Required),
+		validation.Field(&config.MongodbUri, validation.Required),
+		validation.Field(&config.MongodbDatabase, validation.Required),
+		validation.Field(&config.UseRedis, validation.Required),
+		validation.Field(&config.RedisDefaultAddress),
+		validation.Field(&config.JWTSecretKey, validation.Required),
+		validation.Field(&config.JWTAccessExpirationMinutes, validation.Required),
+		validation.Field(&config.JWTRefreshExpirationDays, validation.Required),
+		validation.Field(&config.Mode, validation.In("debug", "release")),
+	)
 }
